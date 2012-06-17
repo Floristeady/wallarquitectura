170a72a0c15fd7c73519d12f7772c3a34e434da7
Florencia Rosenfeld <frosenfeld@gmail.com>
2012-06-17 17:59:39 -0500

    CSS y funcionamiento category-proyectos.php

--- category-proyectos.php
+++ category-proyectos.php
@@ -9,36 +9,67 @@
 
 get_header(); ?>
 
-				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '' . $category_description . '';
+		<?php
+			$category_description = category_description();
+			if ( ! empty( $category_description ) )
+				echo '' . $category_description . '';
 
-				/* Run the loop for the category page to output the posts.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-category.php and that will be used instead.
-				
-				get_template_part( 'loop', 'category' ); */
-				?>
-				
-				<ul id="projects">
-				<?php if (have_posts()) : ?>
-				<?php while (have_posts()) : the_post(); ?>
-			        <li class="project-item">
-			           
-			            <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-			            	<span class="over_title"><h4><?php the_title() ?></h4></span>
-			            
-			            	<span class="img"><?php the_post_thumbnail(array(236,232));?></span>
-			         
-			            </a>
-			            
-				     </li>
-			     <?php
-				endwhile; ?>
-				<?php endif; ?>
-				<?php wp_reset_query(); ?>
-				</ul>
+		/* Run the loop for the category page to output the posts.
+		 * If you want to overload this in a child theme then include a file
+		 * called loop-category.php and that will be used instead.
+		
+		get_template_part( 'loop', 'category' ); */
+		?>
+		
+		<?php $my_query = new WP_Query( array( 
+		    'post_type' => 'post', 
+		    'posts_per_page' => 12, 
+		    'order' => 'DESC'
+		) );
+		
+		echo '<ul id="projects">';
+		
+		while ( $my_query->have_posts() ) : $my_query->the_post();
+		
+		    if (  $my_query->current_post == 1  ||  $my_query->current_post == 5 ||  $my_query->current_post == 7 ||  $my_query->current_post == 9 ){ ?>
+		        <li class="project-item">
+		        	<a class="btn_proyecto" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+		        		<span class="over_title"><h4><?php the_title() ?></h4></span>
+		            	<?php //Obtenemos la url de la imagen destacada
+    					$domsxe = simplexml_load_string(get_the_post_thumbnail($post->ID, 'large'));
+    					$thumbnailsrc = "";
+    					if (!empty($domsxe))
+							$thumbnailsrc = $domsxe->attributes()->src;
+						if (!empty($thumbnailsrc)):
+						?>
+			 			<span class='img'><img src='<?php bloginfo('template_url') ?>/scripts/timthumb.php?src=<?php print $thumbnailsrc; ?>&w=464&h=236' border=0 /></span>
+			 			<?php
+			 			endif;
+			 			?>    
+		        	</a></li>
+		    <?php
+		    } else  { ?>
+		       <li class="project-item">
+		        	<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+		        		<span class="over_title"><h4><?php the_title() ?></h4></span>
+		            	<?php //Obtenemos la url de la imagen destacada
+    					$domsxe = simplexml_load_string(get_the_post_thumbnail($post->ID, 'large'));
+    					$thumbnailsrc = "";
+    					if (!empty($domsxe))
+							$thumbnailsrc = $domsxe->attributes()->src;
+						if (!empty($thumbnailsrc)):
+						?>
+			 			<span class='img'><img src='<?php bloginfo('template_url') ?>/scripts/timthumb.php?src=<?php print $thumbnailsrc; ?>&w=236&h=236' border=0 /></span>
+			 			<?php
+			 			endif;
+			 			?>    
+		        	</a></li>
 
+		  <?php   }
+		
+		endwhile;
+		
+		echo '</ul>';?>
+		<?php wp_reset_query(); ?>
 
 <?php get_footer(); ?>
